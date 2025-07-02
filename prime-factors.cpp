6b60670d8e867e9d81a4eca094faeeb3c30682c2
jongmin.lee <znargon@gmail.com>
2025-07-02 10:22:39 +0900

    fourth cycle - until 4

--- prime-factors.cpp
+++ prime-factors.cpp
@@ -5,7 +5,16 @@ class PrimeFactors {
 public:
 	vector<int> of(int num) {
 		vector<int> result = {};
-		if (num > 1 ) result.push_back(num);
+		if (num > 1) {
+			if (num == 4) {
+				while (num % 2 == 0) {
+					result.push_back(2);
+					num /= 2;
+				}
+			}
+			else 
+				result.push_back(num);
+		}
 		return result;
 	}
 };
