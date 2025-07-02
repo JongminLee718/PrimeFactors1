4d45f4241adbca89a8acc3c516b0310b5d3dff5d
jongmin.lee <znargon@gmail.com>
2025-07-02 10:06:10 +0900

    first cycle

--- prime-factors.cpp
+++ prime-factors.cpp
@@ -1,3 +1,11 @@
+#include <vector>
+using std::vector;
+
 class PrimeFactors {
+public:
+	vector<int> of(int num) {
+		vector<int> result;
+		return result;
+	}
 
 };
--- prime-factors_test.cpp
+++ prime-factors_test.cpp
@@ -1,6 +1,16 @@
 #include "gmock/gmock.h"
 #include "prime-factors.cpp"
+#include <vector>
+
+using std::vector;
+
 
 TEST(PrimeFactors, PrimeTest) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(PrimeFactors, Of1) {
+	PrimeFactors primeFactor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, primeFactor.of(1));
 }
